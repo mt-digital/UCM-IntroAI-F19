17d4bada3245822a838757dca3eecf20b313a37a
Matthew Turner <maturner01@gmail.com>
2019-09-06 11:02:35 -0700

    updated HashSetExample

--- Lab1-Intro/HashSetExample.java
+++ Lab1-Intro/HashSetExample.java
@@ -7,38 +7,84 @@
  */
 
 import java.util.HashSet;
+import java.util.ArrayList;
 
 class HashSetExample {
     public static void main (String[] args) {
-        // Create a new, empty HashSet. HashSet is a Java "generic" container 
+        	
+    	// How to define primitive data type variables:
+    	int x = 1;
+    	int y = 2;
+    	int z = x * y;
+    	System.out.printf("z = %d\n\n", z);
+    	
+    	double pi = 3.14159;
+    	double twicePi = 2 * pi;
+    	System.out.printf("2π = %.5f\n\n", twicePi);
+    	
+    	String s = "hello, world!";
+    	System.out.println(s);
+    	
+    	// Define an ArrayList to put city names into.
+    	ArrayList<String> citiesList = new ArrayList<String>();
+        
+        // Put some California city names in the list.
+        citiesList.add("Merced");
+        citiesList.add("Fresno");
+        citiesList.add("San Luis Obispo");
+        citiesList.add("San Francisco");
+        citiesList.add("Los Angeles");
+        	
+        // Print the list to terminal output.
+    	System.out.println("\nThe cities list, in same order as they were added:");
+    	System.out.println(citiesList);
+    	System.out.println();
+        
+        // 	Create a new, empty HashSet. HashSet is a Java "generic" container 
         // and we should provide the type of the elements we will put in the container.
         HashSet<String> citiesSet = new HashSet<String>();
         
-        // Put some California city names in the set.
-        citiesSet.add("Merced");
-        citiesSet.add("Fresno");
-        citiesSet.add("San Luis Obispo");
-        citiesSet.add("San Francisco");
-        citiesSet.add("Los Angeles");
-
+        // Use Java for (each) notation to iterate over cities in list.
+        for (String city : citiesList) {
+        		citiesSet.add(city);
+        }
+        	// alternative method:
+        HashSet<String> citiesSet2 = new HashSet<String>();
+        for (int i=0; i < citiesList.size(); i++) { 
+    		String thisCity = citiesList.get(i);
+    		citiesSet2.add(thisCity);
+        }
+        
         // Check if a two city is in the set and add it if it is not.
         boolean fresnoInSet = citiesSet.add("Fresno");
+        
+        // We know Fresno is already in the set so we expect "Fresno already in citiesSet!" 
+        // to be printed out.
         if (fresnoInSet) {
             System.out.println("Added Fresno!");
         }
         else {
-            System.out.println("Fresno already there!");
+            System.out.println("Fresno already in citiesSet!");
         }
-
+        
+        // This time we expect the conditional to evaluate to True, and "Added Santa Cruz!"
+        // to be printed out.
         if (citiesSet.add("Santa Cruz")) {
             System.out.println("Added Santa Cruz!");
         }
         else {
-            System.out.println("Santa Cruz already there!");
+            System.out.println("Santa Cruz already in citiesSet!");
         }
-
+        
+        // Another conditional statement but this time using HashSet.contains member function.
         if (citiesSet.contains("San Luis Obispo") && citiesSet.contains("San Francisco")) {
             System.out.println("Two saintly California city names present in set");
         }
+        
+        	System.out.println(citiesSet);
+        	
+        	
+        	
+//        	System.out.println(citiesSet2);
     }
 }
